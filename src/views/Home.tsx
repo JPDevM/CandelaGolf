70f5fc4961bd1320b3144347dc6130501f1b02d9
JPDevM <devecchi@exxo.es>
2022-05-20 22:35:37 +0200

    build

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,13 +1,11 @@
 import React, { Fragment } from 'react';
 
-import Youtube from '../components/Youtube'
+import Youtube from '../components/Youtube';
 
 import Profile1 from '../images/profile/profile-1.jpeg';
 
-
-
 const Home = () => {
-const API_KEY = 'AIzaSyClqThNUw8LgdC5Ckt6AZ6iJ801myDGyKY';
+  const API_KEY = 'AIzaSyClqThNUw8LgdC5Ckt6AZ6iJ801myDGyKY';
   return (
     <Fragment>
       <main>
@@ -26,9 +24,16 @@ const API_KEY = 'AIzaSyClqThNUw8LgdC5Ckt6AZ6iJ801myDGyKY';
                   <h1 className="text-black font-semibold font-mono text-5xl tracking-wide text-center underline decoration-pink-500 decoration-4">
                     Juan Pablo Candela
                   </h1>
-                  <p className='pt-4'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et atque deserunt sint perspiciatis veniam placeat officiis voluptatibus enim tempore, officia ratione fugiat excepturi alias eaque repudiandae, vero reprehenderit error cum!</p>
-                <div className="p-8">
-                <Youtube />
+                  <p className="pt-4">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et
+                    atque deserunt sint perspiciatis veniam placeat officiis
+                    voluptatibus enim tempore, officia ratione fugiat excepturi
+                    alias eaque repudiandae, vero reprehenderit error cum!
+                  </p>
+                  <div className="p-8">
+                    <Youtube />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
