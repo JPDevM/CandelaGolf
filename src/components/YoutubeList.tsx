59d4919cfcffabda5f5c7d1fba1ff40898211e2b
JPDevM <devecchi@exxo.es>
2022-05-26 20:22:07 +0200

    impruve fetch youtube

--- src/components/YoutubeList.tsx
+++ src/components/YoutubeList.tsx
@@ -3,36 +3,45 @@ import React, { Fragment } from 'react';
 
 // Youtube API 3 get latest videos
 // https://www.googleapis.com/youtube/v3/search?part=snippet&channelId={CHANNEL_ID}&maxResults={RESULTS}&order=date&type=video&key={YOUR_API_KEY}
-// https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC8GCujkk3SVrAuUIZrnTsfA&maxResults=4&order=date&type=video&key=AIzaSyDjOdjKdRTQBwa2JQ382IJF54BKGcpy8vc 
-const YOUTUBE_SEARCH_ITEMS_API =
-  'https://www.googleapis.com/youtube/v3/search';
+// https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC8GCujkk3SVrAuUIZrnTsfA&maxResults=4&order=date&type=video&key=AIzaSyDjOdjKdRTQBwa2JQ382IJF54BKGcpy8vc
+const YOUTUBE_SEARCH_ITEMS_API = 'https://www.googleapis.com/youtube/v3/search';
 const CHANNEL_ID = 'UC8GCujkk3SVrAuUIZrnTsfA';
 const RESULTS = 4;
 
-console.log(import.meta.env.VITE_YOUTUBE_APY_KEY);
+// console.log('VITE_YOUTUBE_APY_KEY: ', import.meta.env.VITE_YOUTUBE_APY_KEY);
 
 export const YoutubeList = () => {
-
   // let getData = async () => {
-  //   const response = await fetch("https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC8GCujkk3SVrAuUIZrnTsfA&maxResults=4&order=date&type=video&key=AIzaSyDjOdjKdRTQBwa2JQ382IJF54BKGcpy8vc");
-    // const rersponse = await fetch(`${YOUTUBE_SEARCH_ITEMS_API}?part=snippet&channelId=${CHANNEL_ID}&maxResults=${RESULTS}&order=date&type=video&key=${import.meta.env.VITE_YOUTUBE_APY_KEY}`);
-    // console.log('response: ', response);
- 
-    // const Data = await response.json();
-    // console.log('Data: ', Data);
-    // return Data
-    // return Data
-  // }
-  // let FinalData = getData().then(data => { console.log('data: ', JSON.stringify(data))});
-  // console.log('FinalData: ', FinalData);
-  // console.log('FinalData: ', FinalData.items[0]);
+    // String final
+    // const response = await fetch("https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC8GCujkk3SVrAuUIZrnTsfA&maxResults=4&order=date&type=video&key=AIzaSyDjOdjKdRTQBwa2JQ382IJF54BKGcpy8vc");
+  
+  //   const response = await fetch(
+  //     `${YOUTUBE_SEARCH_ITEMS_API}?part=snippet&channelId=${CHANNEL_ID}&maxResults=${RESULTS}&order=date&type=video&key=${
+  //       import.meta.env.VITE_YOUTUBE_APY_KEY
+  //     }`
+  //   );
+  //   console.log('response: ', response);
+
+  //   const Data = await response.json();
+  //   console.log('Data: ', Data);
+  //   return Data;
+  //   // return Data
+  // };
+  // const links2 = getData().then((data) => {
+  //   var YoutubeID = [];
+  //   for (let i = 0; i < RESULTS; i++) {
+  //     YoutubeID.push('https://www.youtube.com/embed/'+data.items[i].id.videoId);
+  //   }
+  //   console.log('YoutubeID (for): ', YoutubeID);
+  //   return YoutubeID;
+  // });
+  // console.log('links: ', links2);
 
   const links = [
     'https://www.youtube.com/embed/_I26FAHxVEY',
     'https://www.youtube.com/embed/7f_LjrrAuoo',
     'https://www.youtube.com/embed/zND-seMFALA',
     'https://www.youtube.com/embed/WbBbsPDF5OQ',
-
   ];
   return (
     <Fragment>
